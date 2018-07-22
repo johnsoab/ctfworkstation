93936fd00d2ad04af5bf7b9fb595a77c19f047b1
Andy <spydum@gmail.com>
2018-07-22 13:26:49 -0400

    Updated to no-wait, because serialized is dumb for this operation

--- controlall.sh
+++ controlall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Give me deallocate | stop | start as an arg..
-az vm $1 --ids $(az vm list --query "[].id" -o tsv | grep ctfwrk)
+az vm $1 --no-wait --ids $(az vm list --query "[].id" -o tsv | grep ctfwrk)
